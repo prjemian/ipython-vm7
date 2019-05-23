a1e43a68a7472925df6082858d39054d55443539
Pete Jemian <prjemian@gmail.com>
2019-05-23 15:40:04 -0500

    modernize

--- profile_bluesky/startup/60-metadata.py
+++ profile_bluesky/startup/60-metadata.py
@@ -11,12 +11,12 @@ RE.md['pid'] = os.getpid()
 HOSTNAME = socket.gethostname() or 'localhost' 
 USERNAME = getpass.getuser() or 'synApps_xxx_user' 
 RE.md['login_id'] = USERNAME + '@' + HOSTNAME
-RE.md['BLUESKY_VERSION'] = bluesky.__version__
-RE.md['OPHYD_VERSION'] = ophyd.__version__
+RE.md['versions'] = {}
+RE.md['versions']['bluesky'] = bluesky.__version__
+RE.md['versions']['ophyd'] = ophyd.__version__
 from apstools import __version__ as apstools_version
-RE.md['APSTOOLS_VERSION'] = apstools_version
+RE.md['versions']['apstools'] = apstools_version
 del apstools_version
+RE.md['versions']['epics'] = epics.__version__
 
-print("Metadata dictionary:")
-for k, v in sorted(RE.md.items()):
-    print("RE.md['%s']" % k, "=", v)
+print_RE_md()
