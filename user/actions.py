7dbaf8a771ad482efe6bbc3c92d672409c1d5b4c
Pete Jemian <prjemian@gmail.com>
2019-09-12 17:59:51 -0500

    example plan like 8IDI

--- user/actions.py
+++ user/actions.py
@@ -5,15 +5,41 @@ globals().update(IPython.get_ipython().user_ns)
 logger.info(__file__)
 
 
-def action1():
-    APS_utils.show_ophyd_symbols()
-    print(scaler.read())
+def my_scan(count_time=1.0):
+    mot = m1
+    scaler.stage_sigs["preset_time"] = count_time
+    scaler.stage_sigs["count_mode"] = "OneShot"
 
-def catalog():
-    table = pyRestTable.Table()
-    table.labels = "symbol type".split()
-    for k, v in sorted(globals().items()):
-        if k.startswith("_") or k in ("In",):
-            continue
-        table.addRow((k, type(v)))
-    print(table)
+    peaky = calcouts.calcout9
+    calc = calcouts.calcout10
+    peaky.reset()
+    calc.reset()
+
+    md = {
+        "count_time": count_time,
+        "title": "monitor example",
+    }
+
+    monitored_signals = [
+        calc.calculated_value,
+        peaky.calculated_value,
+        clock,
+        I0,
+        scint,
+        I0Mon,
+        # noisy,
+    ]
+    APS_synApps.setup_incrementer_calcout(calc)
+    APS_synApps.setup_lorentzian_calcout(peaky, mot.user_readback, center=-1.4, width=0.3, scale=10)
+
+    #@bpp.stage_decorator([calc])
+    @bpp.monitor_during_decorator(monitored_signals)
+    def inner():
+        yield from bp.scan([peaky.calculated_value], mot, -2, 0, 41, md=md)
+        h = db[-1]
+        print(f"scan_id={RE.md['scan_id']}")
+        print(f"uid={h.start['uid']}")
+        # TODO: call PeakStats
+        calc.reset()
+
+    return (yield from inner())
