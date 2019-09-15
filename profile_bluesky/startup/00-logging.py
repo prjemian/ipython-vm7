574e39d4ec2bfcd9f7cc9f06941416a96004de96
Pete Jemian <prjemian@gmail.com>
2019-09-15 17:55:00 -0500

    use new stdlogpj package

--- profile_bluesky/startup/00-logging.py
+++ profile_bluesky/startup/00-logging.py
@@ -4,8 +4,10 @@ print(__file__)
 configure logging
 """
 
-LOGGER_FILE = ".ipython_logger.log"
-CONSOLE_IO_FILE = ".ipython_console.log"
+_log_path = os.path.join(os.getcwd(), ".logs")
+if not os.path.exists(_log_path):
+    os.mkdir(_log_path)
+CONSOLE_IO_FILE = os.path.join(_log_path, ".ipython_console.log")
 
 # start logging console to file
 # https://ipython.org/ipython-doc/3/interactive/magics.html#magic-logstart
@@ -15,38 +17,11 @@ _ipython = get_ipython()
 _ipython.magic(f"logstart -o -t {CONSOLE_IO_FILE} rotate")
 
 
-# Uncomment the following lines to turn on 
-# verbose messages for debugging.
-import logging
-#logging.basicConfig(level=logging.DEBUG)
-logger = logging.getLogger("bluesky-ipython-shell")
-logger.setLevel(logging.DEBUG)
-#ophyd.logger.setLevel(logging.DEBUG)
+# pip install stdlogpj
+import stdlogpj
+logger = stdlogpj.standard_logging_setup(
+    "bluesky-ipython-shell", "ipython_logger")
 
-stderr_log_handler = logging.StreamHandler()
-logger.addHandler(stderr_log_handler)
-
-# nice output format
-# https://docs.python.org/3/library/logging.html#logrecord-attributes
-stderr_log_format = "%(levelname)-.1s"        # only first letter
-stderr_log_format += " %(asctime)s"
-stderr_log_format += " - "
-stderr_log_format += "%(message)s"
-stderr_log_handler.setFormatter(logging.Formatter(stderr_log_format))
-stderr_log_handler.formatter.default_msec_format = "%s.%03d"
-
-file_log_handler = logging.FileHandler(LOGGER_FILE)
-logger.addHandler(file_log_handler)
-file_log_format = "%(asctime)s"
-file_log_format += ",%(levelname)s"
-file_log_format += ",%(name)s"
-file_log_format += ",%(module)s"
-file_log_format += ",%(lineno)d"
-file_log_format += ",%(threadName)s"
-file_log_format += " - "
-file_log_format += "%(message)s"
-file_log_handler.setFormatter(logging.Formatter(file_log_format))
-file_log_handler.formatter.default_msec_format = "%s.%03d"
 
 logger.info('#'*60 + " startup")
 logger.info('logging started')
