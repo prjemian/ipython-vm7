85a9bb2d693df0f795f384e83a68199a6c833c14
Pete Jemian <prjemian@gmail.com>
2019-12-04 07:57:57 -0600

    pull the weeds

--- profile_bluesky/startup/02-pyepics.py
+++ profile_bluesky/startup/02-pyepics.py
@@ -10,13 +10,6 @@ import epics
 up = epics.caget("sky:UPTIME", timeout=1)
 if up is None:
     logger.info("IOCs not running.  Starting them now...")
-    # ~/bin/start_iocs.sh
     start_ioc_script = "/home/mintadmin/bin/start_iocs.sh"
-    # import subprocess
-    # process = subprocess.Popen(
-    #     start_ioc_script, 
-    #     shell=True, stdout=subprocess.PIPE)
-    # process.wait()
-    # logger.debug(f"{process.returncode}")
     os.system(start_ioc_script)
     logger.debug("IOCs started")
