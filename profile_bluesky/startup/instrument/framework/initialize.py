54a7fb98c6344ce087a8bbcefaa7a644ebb0c6c8
Pete Jemian <prjemian@gmail.com>
2020-02-13 00:16:51 -0600

    agnostic to MPL backend now

--- profile_bluesky/startup/instrument/framework/initialize.py
+++ profile_bluesky/startup/instrument/framework/initialize.py
@@ -58,8 +58,8 @@ from bluesky.callbacks.broker import verify_files_saved
 # callback_db['post_run_verify'] = RE.subscribe(post_run(verify_files_saved), 'stop')
 
 # Make plots update live while scans run.
-from bluesky.utils import install_qt_kicker
-install_qt_kicker()
+from bluesky.utils import install_kicker
+install_kicker()
 
 # convenience imports
 # from bluesky.callbacks import *
