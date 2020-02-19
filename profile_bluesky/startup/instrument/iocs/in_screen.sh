67ee6c6f4dff5f4fcea20c5c8ee1132ef17d760b
Pete Jemian <prjemian@gmail.com>
2020-02-19 10:09:44 -0600

    where to get softIoc?

--- profile_bluesky/startup/instrument/iocs/in_screen.sh
+++ profile_bluesky/startup/instrument/iocs/in_screen.sh
@@ -3,7 +3,7 @@
 # in_screen.sh
 
 export BASE=$(dirname $0)
-export CAGET=/usr/local/epics/base/bin/linux-x86_64/caget
+# export CAGET=/usr/local/epics/base/bin/linux-x86_64/caget
 
 /usr/bin/screen -d -m -S IOC_registers -h 5000 ${BASE}/run_ioc.sh
 
