c71099eb5eae5e545c352dbc4997fefb88c17888
Pete Jemian <prjemian@gmail.com>
2020-02-19 10:16:58 -0600

    research

--- profile_bluesky/startup/instrument/iocs/in_screen.sh
+++ profile_bluesky/startup/instrument/iocs/in_screen.sh
@@ -8,3 +8,23 @@ export BASE=$(dirname $0)
 /usr/bin/screen -d -m -S IOC_registers -h 5000 ${BASE}/run_ioc.sh
 
 echo `screen -ls`
+
+# Where to get softIoc?
+# 
+# of these:
+# (base) prjemian@ookhd:~/.ipython$ locate softIoc | grep /bin/
+# /home/prjemian/Apps/miniconda3/envs/epics/epics/bin/linux-x86_64/softIoc
+# /home/prjemian/Apps/miniconda3/pkgs/epics-base-3.15.6-he1b5a44_0/epics/bin/linux-x86_64/softIoc
+# /home/prjemian/Apps/miniconda3/pkgs/epics-base-7.0.3.1-pl526_1/bin/softIoc
+# /home/prjemian/Apps/miniconda3/pkgs/epics-base-7.0.3.1-pl526_1/bin/softIocPVA
+# /home/prjemian/Apps/miniconda3/pkgs/epics-base-7.0.3.1-pl526_1/opt/epics/bin/linux-x86_64/softIoc
+# /home/prjemian/Apps/miniconda3/pkgs/epics-base-7.0.3.1-pl526_1/opt/epics/bin/linux-x86_64/softIocPVA
+#
+# this one works: epics-base-3.15.6-he1b5a44_0
+# (same as /home/prjemian/Apps/miniconda3/envs/epics/epics/bin/linux-x86_64/softIoc)
+# 
+# the others have problems:
+# (base) prjemian@ookhd:~/.ipython$ /home/prjemian/Apps/miniconda3/pkgs/epics-base-7.0.3.1-pl526_1/bin/softIoc
+# /home/prjemian/Apps/miniconda3/pkgs/epics-base-7.0.3.1-pl526_1/bin/softIoc: error while loading shared libraries: libdbRecStd.so.3.17.0: cannot open shared object file: No such file or directory
+# (base) prjemian@ookhd:~/.ipython$ /home/prjemian/Apps/miniconda3/pkgs/epics-base-7.0.3.1-pl526_1/bin/softIocPVA
+# /home/prjemian/Apps/miniconda3/pkgs/epics-base-7.0.3.1-pl526_1/bin/softIocPVA: error while loading shared libraries: libqsrv.so.1.1: cannot open shared object file: No such file or directory
