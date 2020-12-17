d1a5cddfd62bf18d3cd8583123bce7e87f7bb686
Pete Jemian <prjemian@gmail.com>
2020-12-16 20:47:58 -0600

    for ophyd 1.6.09 release

--- profile_bluesky/startup/instrument/framework/initialize.py
+++ profile_bluesky/startup/instrument/framework/initialize.py
@@ -1,30 +1,60 @@
-
 """
 initialize the bluesky framework
 """
 
 __all__ = [
-    'RE', 'callback_db', 'db', 'sd',
-    'bec', 'peaks',
-    'bp', 'bps', 'bpp',
-    'np',
-    'summarize_plan',
-    ]
+    "bec",
+    "bp",
+    "bpp",
+    "bps",
+    "BS_CATALOG_NAME",
+    "callback_db",
+    "db",
+    "np",
+    "peaks",
+    "RE",
+    "sd",
+    "summarize_plan",
+]
 
 from ..session_logs import logger
+
 logger.info(__file__)
+
+from bluesky import RunEngine
+from bluesky import SupplementalData
+from bluesky.callbacks.best_effort import BestEffortCallback
+from bluesky.callbacks.broker import verify_files_saved
+from bluesky.magics import BlueskyMagics
+from bluesky.simulators import summarize_plan
+from bluesky.utils import get_history
+from bluesky.utils import PersistentDict
+from bluesky.utils import ProgressBarManager
+from bluesky.utils import ts_msg_hook
+from IPython import get_ipython
+from ophyd.signal import EpicsSignalBase
+import databroker
+import ophyd
 import os
 import sys
 
-from bluesky import RunEngine
-from bluesky.utils import PersistentDict
+# convenience imports
+import bluesky.plans as bp
+import bluesky.plan_stubs as bps
+import bluesky.preprocessors as bpp
+import numpy as np
+
+
+# BS_CATALOG_NAME = "mongodb_config"
+BS_CATALOG_NAME = "bs2021"
+
 
 def get_md_path():
     md_dir_name = "Bluesky_RunEngine_md"
     if os.environ == "win32":
         home = os.environ["LOCALAPPDATA"]
         path = os.path.join(home, md_dir_name)
-    else:       # at least on "linux"
+    else:  # at least on "linux"
         home = os.environ["HOME"]
         path = os.path.join(home, ".config", md_dir_name)
     return path
@@ -35,14 +65,14 @@ old_md = None
 md_path = get_md_path()
 if not os.path.exists(md_path):
     logger.info(
-        "New directory to store RE.md between sessions: %s", 
-        md_path)
+        "New directory to store RE.md between sessions: %s", md_path
+    )
     os.makedirs(md_path)
     from bluesky.utils import get_history
+
     old_md = get_history()
 
 # Set up a RunEngine and use metadata backed PersistentDict
-from bluesky.utils import get_history
 RE = RunEngine({})
 RE.md = PersistentDict(md_path)
 if old_md is not None:
@@ -52,59 +82,52 @@ if old_md is not None:
 # keep track of callback subscriptions
 callback_db = {}
 
-# Set up a Broker.
-from databroker import Broker
-db = Broker.named('mongodb_config')
+# Connect with mongodb
+# db = databroker.Broker.named('mongodb_config')
+db = databroker.catalog[BS_CATALOG_NAME]
 
 # Subscribe metadatastore to documents.
 # If this is removed, data is not saved to metadatastore.
-callback_db['db'] = RE.subscribe(db.insert)
+callback_db["db"] = RE.subscribe(db.v1.insert)
 
 # Set up SupplementalData.
-from bluesky import SupplementalData
 sd = SupplementalData()
 RE.preprocessors.append(sd)
 
 # Add a progress bar.
-from bluesky.utils import ProgressBarManager
 pbar_manager = ProgressBarManager()
 RE.waiting_hook = pbar_manager
 
 # Register bluesky IPython magics.
-from IPython import get_ipython
-from bluesky.magics import BlueskyMagics
 get_ipython().register_magics(BlueskyMagics)
 
 # Set up the BestEffortCallback.
-from bluesky.callbacks.best_effort import BestEffortCallback
 bec = BestEffortCallback()
-callback_db['bec'] = RE.subscribe(bec)
+callback_db["bec"] = RE.subscribe(bec)
 peaks = bec.peaks  # just as alias for less typing
 bec.disable_baseline()
 
-# At the end of every run, verify that files were saved and
-# print a confirmation message.
-from bluesky.callbacks.broker import verify_files_saved
-# callback_db['post_run_verify'] = RE.subscribe(post_run(verify_files_saved), 'stop')
+# uncomment: Verify that files saved & print confirmation message.
+# _sub = RE.subscribe(post_run(verify_files_saved), 'stop')
+# callback_db['post_run_verify'] = _sub
 
-# convenience imports
-# from bluesky.callbacks import *
-# from bluesky.callbacks.broker import *
-# from bluesky.simulators import *
-import bluesky.plans as bp
-import bluesky.plan_stubs as bps
-import bluesky.preprocessors as bpp
-import numpy as np
 
-# Uncomment the following lines to turn on 
-# verbose messages for debugging.
+# Uncomment enable verbose debugging messages.
 # ophyd.logger.setLevel(logging.DEBUG)
 
 # diagnostics
-from bluesky.utils import ts_msg_hook
-#RE.msg_hook = ts_msg_hook
-from bluesky.simulators import summarize_plan
+# RE.msg_hook = ts_msg_hook
 
 # set default timeout for all EpicsSignal connections & communications
-import ophyd
-ophyd.EpicsSignal.set_default_timeout(timeout=10, connection_timeout=5)
+if hasattr(EpicsSignalBase, "set_defaults"):
+    # ophyd 1.6.0+
+    EpicsSignalBase.set_defaults(
+        auto_monitor=True,
+        connection_timeout=5,
+        timeout=10,
+        write_timeout=5,
+    )
+else:
+    EpicsSignalBase.set_default_timeout(
+        connection_timeout=5, timeout=10,
+    )
