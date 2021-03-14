79f3bee8a516464b99bb17890150473579bb824b
Pete Jemian <prjemian@gmail.com>
2021-03-13 18:21:08 -0600

    clean up adrea detectors

--- profile_bluesky/startup/instrument/devices/simdet.py
+++ profile_bluesky/startup/instrument/devices/simdet.py
@@ -1,23 +1,11 @@
 
 """
 area detectors: ADSimDetector
-
-Note these symbols have not connected:
-
-    timeout: adsimdet_cam_pool_max_buffers
-    timeout: adsimdet_image_pool_max_buffers
-    timeout: adsimdet_hdf1_pool_max_buffers
-    timeout: adsimdet_hdf1_file_number_sync
-    timeout: adsimdet_hdf1_file_number_write
-    timeout: adsimdet_tiff1_pool_max_buffers
-    timeout: adsimdet_tiff1_file_number_sync
-    timeout: adsimdet_tiff1_file_number_write
-
 """
 
 __all__ = [
     'adsimdet',
-    'altsimdet',
+    # 'altsimdet',
 ]
 
 import apstools.devices
@@ -26,7 +14,8 @@ from ophyd.areadetector import EpicsSignalWithRBV
 from ophyd.areadetector import ADComponent
 from ophyd.areadetector import HDF5Plugin
 from ophyd.areadetector import ImagePlugin
-from ophyd.areadetector import SimDetector
+from ophyd.areadetector import AreaDetector
+from ophyd.areadetector import SimDetectorCam
 from ophyd.areadetector import SingleTrigger
 from ophyd.areadetector import TIFFPlugin
 from ophyd.areadetector.filestore_mixins import FileStoreHDF5IterativeWrite
@@ -54,43 +43,54 @@ class MyHDF5Plugin(HDF5Plugin, FileStoreHDF5IterativeWrite):
     array_callbacks = Component(EpicsSignalWithRBV, suffix="ArrayCallbacks")
 
     pool_max_buffers = None
+    file_number_sync = None
+    file_number_write = None
 
 class MyTIFFPlugin(TIFFPlugin, FileStoreTIFFIterativeWrite):
     # create_directory_depth = Component(EpicsSignalWithRBV, suffix="CreateDirectory")
 
     pool_max_buffers = None
+    file_number_sync = None
+    file_number_write = None
+
+class MyCam(SimDetectorCam):
+    pool_max_buffers = None
+
+class MyImagePlugin(ImagePlugin):
+    pool_max_buffers = None
 
 
-class MySingleTriggerSimDetector(SingleTrigger, SimDetector): 
-       
-    image = Component(ImagePlugin, suffix="image1:")
+class MySingleTriggerSimDetector(SingleTrigger, AreaDetector):
+
+    cam = ADComponent(MyCam, suffix="cam1:")
+    image = Component(MyImagePlugin, suffix="image1:")
     hdf1 = ADComponent(
         MyHDF5Plugin,
-        suffix='HDF1:', 
+        suffix='HDF1:',
         root=DATABROKER_ROOT_PATH,
         write_path_template = WRITE_HDF5_FILE_PATH,
     )
     tiff1 = Component(
         MyTIFFPlugin,
-        suffix='TIFF1:', 
+        suffix='TIFF1:',
         write_path_template = WRITE_HDF5_FILE_PATH,
     )
 
-
 class myHdf5EpicsIterativeWriter(
-    apstools.devices.AD_EpicsHdf5FileName, 
+    apstools.devices.AD_EpicsHdf5FileName,
     FileStoreIterativeWrite): pass
 class myHDF5FileNames(HDF5Plugin, myHdf5EpicsIterativeWriter): pass
 class EpicsDefinesHDF5FileNames(HDF5Plugin, myHdf5EpicsIterativeWriter):
     create_directory_depth = Component(EpicsSignalWithRBV, suffix="CreateDirectory")
 
 
-class MyRenegadeSimDetector(SingleTrigger, SimDetector): 
-       
-    image = ADComponent(ImagePlugin, suffix="image1:")
+class MyRenegadeSimDetector(SingleTrigger, AreaDetector):
+
+    cam = ADComponent(MyCam, suffix="cam1:")
+    image = ADComponent(MyImagePlugin, suffix="image1:")
     hdf1 = ADComponent(
         EpicsDefinesHDF5FileNames,
-        suffix = "HDF1:", 
+        suffix = "HDF1:",
         root = DATABROKER_ROOT_PATH,
         write_path_template = WRITE_HDF5_FILE_PATH,
         # read_path_template = READ_HDF5_FILE_PATH,
@@ -109,17 +109,17 @@ try:
 except TimeoutError:
     logger.error(f"Could not connect {_ad_prefix} sim detector as 'adsimdet'")
 
-try:
-    # EPICS chooses file names with this detector
-    altsimdet = MyRenegadeSimDetector(_ad_prefix, name='altsimdet')
-    altsimdet.read_attrs.append("hdf1")
-    if altsimdet.hdf1.create_directory_depth.get() == 0:
-        # set it now to some default
-        altsimdet.hdf1.create_directory_depth.put(-5)
-    altsimdet.hdf1.file_path.put(WRITE_HDF5_FILE_PATH)
-    altsimdet.hdf1.file_name.put("navy")
-    altsimdet.hdf1.file_number.put(101)
-    altsimdet.hdf1.array_counter.put(altsimdet.hdf1.file_number.get())
+# try:
+#     # EPICS chooses file names with this detector
+#     altsimdet = MyRenegadeSimDetector(_ad_prefix, name='altsimdet')
+#     altsimdet.read_attrs.append("hdf1")
+#     if altsimdet.hdf1.create_directory_depth.get() == 0:
+#         # set it now to some default
+#         altsimdet.hdf1.create_directory_depth.put(-5)
+#     altsimdet.hdf1.file_path.put(WRITE_HDF5_FILE_PATH)
+#     altsimdet.hdf1.file_name.put("navy")
+#     altsimdet.hdf1.file_number.put(101)
+#     altsimdet.hdf1.array_counter.put(altsimdet.hdf1.file_number.get())
 
-except TimeoutError:
-    logger.error(f"Could not connect {_ad_prefix} sim detector as 'altsimdet'")
+# except TimeoutError:
+#     logger.error(f"Could not connect {_ad_prefix} sim detector as 'altsimdet'")
